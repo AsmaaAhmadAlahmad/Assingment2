3f783c8c6067dd7a0148e2c5fcba600b8e127944
AsmaaAlAhmad <asmaaahmadalahmad1997@gmail.com>
2024-02-08 15:36:39 +0300

    تصحيح خطا

--- Assingment2App/Program.cs
+++ Assingment2App/Program.cs
@@ -13,7 +13,7 @@ namespace Assingment2App
 
             //ماتم طلبه في الطلب الاخير 
 
-            ////اضافة مصنع 
+            // انشاء مصنع لإضافته  
             Supplier supplier1 = new Supplier()
             {
                 Name = "YousefAlahmed",
@@ -27,43 +27,71 @@ namespace Assingment2App
                   }
             };
             SupplierHandler supplierHandler1 = new SupplierHandler();
-            supplierHandler1.AddSupplier(supplier1);
+            supplierHandler1.AddSupplier(supplier1); // إضافة المصنع
 
 
-            ////اضافة سيارة
+
+
+            // إنشاء سيارة لإضافتها 
             Car car1 = new Car
             {
                 Model = "Honda Accord2005",
                 Gear = "Automatic",
-                Km = 60.43
-                ,
+                Km = 60.43,
                 Year = 2004,
                 Parts = new List<Part>
                 {
-                    new Part {Name="Car Radio" , Price="384$",Quantity=80 ,SupplierId=2},
-                    new Part {Name="Car Glass" , Price="123$",Quantity=40 ,SupplierId=3 } ,
+                    new Part
+                    {
+                        Name    = "Car Radio" ,
+                        Price   = "384$",Quantity=80 ,
+                       Supplier = new Supplier { Name="Asmaa", Address = "Hama"}
+                    },
+
+                    new Part
+                    {
+                        Name="Car Glass",
+                        Price="123$",Quantity=40,
+                        //SupplierId=1,
+                        Supplier = new Supplier { Name="Anas", Address = "Hama"}
+                    } ,
                 }
             };
-            CarHandler carHandler1 = new CarHandler();
-            carHandler1.CarAdd(car1);
 
-            ////اضافة جزء 
+            CarHandler carHandler1 = new CarHandler();
+            carHandler1.CarAdd(car1); // اضافة السيارة
+            carHandler1.GetCars();
+
+
+
+
+            // إنشاء جزء لإضافته
             Part part1 = new Part
             {
                 Name = "Oil Filter2",
                 Price = "200$",
                 Quantity = 324,
-                SupplierId = 5,
+                Supplier = new Supplier { Name = "Ahmed", Address = "Idleb" },
                 Cars = new List<Car>
+                {
+                      new Car
                       {
-                          new Car { Model="Jeep Wrangler2005",Gear="Automatic",Km=40,Year=2005},
-                          new Car { Model="Tesla Model S2006",Gear="Automatic",Km=60,Year=2006}
+                          Model="Jeep Wrangler2005",
+                          Gear="Automatic",
+                          Km=40,Year=2005
+                      },
 
-                   }
-
+                       new Car
+                       {
+                           Model="Tesla Model S2006",
+                           Gear="Automatic",
+                           Km=60,
+                           Year=2006
+                       }
+                }
             };
             PartHandler partHandler1 = new PartHandler();
-            partHandler1.AddParts(part1);
+            partHandler1.AddParts(part1); // إضافة الجزء
 
 
 
@@ -100,116 +128,96 @@ namespace Assingment2App
 
 
 
-            //            //this is test 
-            //            //for the car class
+            ////            //this is test 
+            ////            //for the car class
 
-            //            Car car = new Car
-            //            {
-            //                Model = "Audi A1",
-            //                Gear = "Automatic",
-            //                Km = 100.43,
-            //                Parts = new List<Part>
-            //                {
-            //                    new Part {Name="Engine" , Price="800",Quantity=30 ,SupplierId=1},
-            //                    new Part {Name="light" , Price="200$",Quantity=10 ,SupplierId=1 } ,
-            //                }
-            //            };
-            //            CarHandler carHandler = new CarHandler();
-            //            carHandler.GetCars();
-            //            carHandler.CarDelete("Audi A4");
-            //            carHandler.CarAdd(car);
-            //            carHandler.CarUpdate("model", " Range Rover", " Range Rover2009");
-
-
-
-            //        //    for the customer class
-            //             Customer Anas = new Customer
-            //             {
-            //                 Name = "Anas",
-            //                 Address = "Idleb",
-            //                 Age = 28,
-            //                 Sales = new List<Sales>//ارقام السيارات التي لهذا الزبون لايمكنني وضعها لزبون اخر لان 
-            //                                             //العلاقة بين جدول المبيعات والسيارت  1 الى 1  
-            //                                           //يعني بأن السيارة يتم بيعها مرة واحدة
-            //                                           //والا ستظهر رسالة للمستخدم بأن السيارة تم
-            //                                           //بيعها سابقا
-            //                                  {
-            //                     new Sales { Total = "53.2", CarId = 52 },
-            //                     new Sales { Total = "57.2", CarId = 53 }
-
-            //                 }
-
-            //             };
-            //Customer Samer = new Customer//أنشأت هذا الزبون لكني لم اضيفه بعد
+            //Car car = new Car
             //{
-            //    Name = "Samer",
+            //    Model = "Audi A1",
+            //    Gear = "Automatic",
+            //    Km = 100.43,
+            //    Parts = new List<Part>
+            //                {
+            //                    new Part
+            //                    {
+            //                        Name="Engine",
+            //                        Price="800",
+            //                        Quantity=30,
+            //                        Supplier = new Supplier { Name = "Omar", Address = "Aleppo" }
+            //                    },
+            //                }
+            //};
+            //CarHandler carHandler = new CarHandler();
+            //carHandler.GetCars();
+            //carHandler.CarDelete("Audi A4");
+            //carHandler.CarAdd(car);
+            //carHandler.CarUpdate("model", " Range Rover", " Range Rover2009");
+
+
+
+            ////  //    for the customer class
+            //Customer Anas = new Customer
+            //{
+            //    Name = "Anas",
             //    Address = "Idleb",
             //    Age = 28,
-            //    Sales = new List<Sales>//ارقام السيارات التي لهذا الزبون لايمكنني وضعها لزبون اخر لان 
-            //                                             //العلاقة بين جدول المبيعات والسيارت  1 الى 1  
-            //                                           //يعني بأن السيارة يتم بيعها مرة واحدة
-            //                                           //والا ستظهر رسالة للمستخدم بأن السيارة تم
-            //                                           //بيعها سابقا
-            //                                  {
-            //                                      new Sales {Total="53.2" ,CarId=50},
-            //                                      new Sales {Total= "57.2",CarId=51}
-
-            //                                  }
-
+            //    Sales = new List<Sales>
+            //                        {
+            //                           new Sales
+            //                           {
+            //                               Total = "53.2"
+            //                               ,Car  =  new Car
+            //                               {
+            //                                           Model = "Honda Accord2005",
+            //                                           Gear = "Automatic",
+            //                                           Km = 60.43,
+            //                                           Year = 2004,
+            //                               }
+            //                           }
+            //                         }
             //};
+
             //CustomerHandler customerHandler = new CustomerHandler();
             //customerHandler.AddCustomer(Anas);
-            //// customerHandler.UpdateCustomer("Name", "Anas", "Asmaa");
-            //customerHandler.DeleteCustomer("Anas");
+            //customerHandler.UpdateCustomer("Name", "Anas", "Asmaa");
             //customerHandler.GetCustomer();
+            //customerHandler.DeleteCustomer("Anas");
 
 
 
             ////for the part class
-            //                     Part part = new Part
-            //                     {
-            //                         Name = "Wheels",
-            //                         Price = "100$",
-            //                         Quantity = 1000,
-            //                         SupplierId = 1,
-            //                         Cars = new List<Car>
+            //Part part = new Part
+            //{
+            //    Name = "Wheels",
+            //    Price = "100$",
+            //    Quantity = 1000,
+            //    Supplier = new Supplier { Name = "Mohamed", Address = "Homs" },
+            //    Cars = new List<Car>
             //                         {
-            //                                            new Car { Model=" Range Rover",Gear="Automatic",Km=80,Year=2000},
-            //                                            new Car { Model="Mercedes-Benz",Gear="Automatic",Km=80,Year=1993}
-
+            //                            new Car { Model=" Range Rover",Gear="Automatic",Km=80,Year=2000},
+            //                            new Car { Model="Mercedes-Benz",Gear="Automatic",Km=80,Year=1993}
             //                         }
 
-            //                     };
+            //};
             //PartHandler handler = new PartHandler();
-            ////handler.AddParts(part);
-            ////handler.GetParts();
-            ////handler.UpdateParts("Quantity", 1000, 500);
+            //handler.AddParts(part);
+            //handler.GetParts();
+            //handler.UpdateParts("Quantity", 1000, 500);
             //handler.RemoveParts("Wheels");
 
-            ////for the sale class
-            //SalesHandler saleHandler = new SalesHandler();
-            ////saleHandler.GetSales();
-            //Sales sales = new Sales//رقم السيارة التي لعملية البيع هذه لايمكنني وضعها لعملية اخرى لان 
-            //                       //العلاقة بين جدول المبيعات والسيارت  1 الى 1  
-            //                       //يعني بأن السيارة يتم بيعها مرة واحدة فقط
-            //                       //والا ستظهر رسالة للمستخدم بأن السيارة تم
-            //                       //بيعها سابقا
+            //////for the sale class
+            //Sales sales = new Sales
             //{
             //    Total = "234.6",
-            //    CarId = 43,
-            //    CustomerId = 27,
+            //    Car = new Car { Gear = "test ", Km = 43, Model = "test", Year = 2001 },
+            //    Customer = new Customer { Name = "test", Address = "Test" },
             //};
-            //Sales sales2 = new Sales//هذه لم اقم بإضافتها بعد
-            //{
-            //    Total = "234.6",
-            //    CarId = 43,
-            //    CustomerId = 27,
-            //};
-            //saleHandler.AddSales(sales);
-            //saleHandler.UpdateSales("total", "234.6", "547.8");
-            //saleHandler.GetSales();
+            //SalesHandler salesHandler = new SalesHandler();
+            //salesHandler.AddSales(sales);
+            //salesHandler.UpdateSales("total", "234.6", "547.8");
+            //salesHandler.GetSales();
 
-            ////for supplier class
+            //////for supplier class
             //SupplierHandler supplierHandler = new SupplierHandler();
             ////supplierHandler.GetSupplier();
 
